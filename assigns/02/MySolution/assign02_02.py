8c9c43099b9774de8f64b176ced3731cdb60e61b
Afzalkhan377 <97460290+Afzalkhan377@users.noreply.github.com>
2023-02-08 16:31:49 -0500

    not working

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -1,6 +1,7 @@
 ####################################################
 import sys
 sys.path.append('..')
+from assign02_01 import *
 from assign02 import *
 ####################################################
 print("[import ./../assign02.py] is done!")
@@ -10,14 +11,27 @@ print("[import ./../assign02.py] is done!")
 # mylist_quicksort (see list_quicksort in assign02.sml)
 #
 ####################################################
-def mylist_quiclsort(xs):
-    qsort(xs)
 
-def qsort(xs):
-    if  mylist_nilq(xs)==0:
-        return mylist_nil()
+def qpart(xs,p0):
+    if mylist_nilq(xs):
+        return (mylist_nil(),mylist_nil())
     else:
-        ys,zs=qpart(xs,mylist_cons.get_cons1(xs))
-        ys=qsort(ys)
-        zs = qsort(zs)
-        mylist_append(ys, mylist_cons(x1,zs))
+        x1=xs.get_cons1()
+        ys=qpart(xs.get_cons2() , p0)
+        zs=qpart(xs.get_cons2() , p0)
+        if x1 <= p0:
+            return(mylist_cons(x1,ys),zs)
+        else:
+            return (ys,mylist_cons(x1,zs))
+
+        
+def mylist_quicksort(xs):
+       if mylist_nilq(xs):
+           return mylist_nil()
+       else:
+           x1=xs.get_cons1()
+           ys=qpart(xs.get_cons2(),x1)
+           zs=qpart(xs.get_cons2(),x1)
+           ys=mylist_quicksort(ys)
+           zs=mylist_quicksort(zs)
+           return mylist_append(ys, mylist_cons(x1,zs))
